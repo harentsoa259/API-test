3597348727581e5e35e1b46e5b636d094b8614a1
harensoa259 <narovanaharentsoa@gmail.com>
2025-07-24 09:08:33 +0300

    feat:implementation of Q1 and Q2

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse, PlainTextResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/hello")
 def read_root():
-    return {"message": "Bienvenue sur mon API!"}
+    return {"message": "Hello world"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/welcome/{name}")
+def read_item(request: Request, name: str):
+    accept_headers = request.headers.get("Accept")
 
+    if name is None:
+        return {"message": "hello"}
+
+    if accept_headers != "text/plain":
+        return JSONResponse({"message": "Unsupported Media Type"}, status_code=400)
+    else:
+        return PlainTextResponse(f"hello {name}")
 # python -m venv venv
 # venv\Scripts\activate
 # pip install fastapi uvicorn
