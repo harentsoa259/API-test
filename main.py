1dd4c183c0ad27911845ec0399a6f3e43c88f7c8
harensoa259 <narovanaharentsoa@gmail.com>
2025-07-24 09:44:59 +0300

    feat: implementation of exercice

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, PlainTextResponse
+from pydantic import BaseModel
 
 app = FastAPI()
+
+students_db = []
+
+class Student(BaseModel):
+    Reference: str
+    firstName: str
+    lastName: str
+    Age: int
 
 @app.get("/hello")
 def read_root():
@@ -9,15 +18,23 @@ def read_root():
 
 @app.get("/welcome/{name}")
 def read_item(request: Request, name: str):
-    accept_headers = request.headers.get("Accept")
-
-    if name is None:
-        return {"message": "hello"}
-
+    accept_headers = request.headers.get("accept")
     if accept_headers != "text/plain":
-        return JSONResponse({"message": "Unsupported Media Type"}, status_code=400)
-    else:
-        return PlainTextResponse(f"hello {name}")
+        return JSONResponse(content={"message": "Unsupported Media Type"}, status_code=400)
+    return PlainTextResponse(f"hello {name}")
+
+@app.post("/students")
+def add_students(students: list[Student]):
+    for student in students:
+        students_db.append(student)
+    return {"message": "Étudiants ajoutés avec succès", "count": len(students)}
+
+@app.get("/students")
+def get_students():
+    return {"students": students_db}
+
+
+
 # python -m venv venv
 # venv\Scripts\activate
 # pip install fastapi uvicorn
